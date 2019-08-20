b366431f0c7769faf3ca229dad6a58bb3c5cff87
yo7 <bloom6445@gmail.com>
2019-08-20 19:12:02 +0900

    tag group

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import classNames from 'classnames'
 import { Base, BaseProps } from '../../internal/Base'
+import { TagGroup } from './TagGroup'
 
 type TagColor = 'light' | 'transparent'
 
@@ -10,31 +11,30 @@ type Props = {
   prefix?: string
 } & BaseProps<'span' | 'a'>
 
-export const Tag: React.FC<Props> = ({
-  color = 'light',
-  className,
-  ...props
-}) => {
-  const colorName = `is-${color}`
+export const Tag = Object.assign(
+  ({ color = 'light', className, ...props }: Props) => {
+    const colorName = `is-${color}`
 
-  const text = props.prefix
-    ? `${props.prefix} ${props.children}`
-    : props.children
+    const text = props.prefix
+      ? `${props.prefix} ${props.children}`
+      : props.children
 
-  return (
-    <Base
-      as={props.as || 'span'}
-      className={classNames(
-        'pl-tag',
-        colorName,
-        {
-          'has-prefix': props.prefix,
-        },
-        className
-      )}
-      {...props}
-    >
-      {text}
-    </Base>
-  )
-}
+    return (
+      <Base
+        as={props.as || 'span'}
+        className={classNames(
+          'pl-tag',
+          colorName,
+          {
+            'has-prefix': props.prefix,
+          },
+          className
+        )}
+        {...props}
+      >
+        {text}
+      </Base>
+    )
+  },
+  { Group: TagGroup }
+)
