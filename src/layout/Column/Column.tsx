5a92839f6db04143894f0ac7aa61fdef639c5ac5
yo7 <bloom6445@gmail.com>
2019-08-21 18:18:43 +0900

    column gap

--- src/layout/Column/Column.tsx
+++ src/layout/Column/Column.tsx
@@ -4,7 +4,7 @@ import { Base, BaseProps } from '../../internal/Base'
 import { ColumnGroup } from './ColumnGroup'
 
 export type ColumnProps = {
-  size: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12
+  size?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12
 } & BaseProps<'div' | 'span' | 'li' | 'a'>
 
 export const Column = Object.assign(
