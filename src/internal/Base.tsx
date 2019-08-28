cafb56275cbde0db1c25449f9f1084877091963f
yo7 <bloom6445@gmail.com>
2019-08-28 13:07:40 +0900

    enable type completion

--- src/internal/Base.tsx
+++ src/internal/Base.tsx
@@ -1,19 +1,12 @@
 import * as React from 'react'
-import { PartialBy } from '../types'
 
-type InputBaseProps<K extends keyof React.ReactHTML> = {
-  as: K
+export type BaseProps<K extends keyof React.ReactHTML> = {
+  as?: K
   className?: string
   style?: React.CSSProperties
 } & React.ComponentPropsWithoutRef<K>
 
-export type BaseProps<K extends keyof React.ReactHTML> = PartialBy<
-  InputBaseProps<K>,
-  'as'
->
-
-export const Base = <K extends keyof React.ReactHTML>(
-  props: InputBaseProps<K>
-) => {
-  return React.createElement(props.as, props)
+export const Base = <K extends keyof React.ReactHTML>(props: BaseProps<K>) => {
+  const { as: tagAs, ...rest } = props
+  return React.createElement(tagAs || 'div', rest)
 }
