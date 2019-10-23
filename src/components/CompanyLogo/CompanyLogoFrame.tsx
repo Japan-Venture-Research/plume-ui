1e3ca6b7c5173a0472a251971a239af46b51f638
yo7 <bloom6445@gmail.com>
2019-10-23 16:31:43 +0900

    update im

--- src/components/CompanyLogo/CompanyLogoFrame.tsx
+++ src/components/CompanyLogo/CompanyLogoFrame.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react'
 import { Base, BaseProps } from '../../internal/Base'
 import classNames from 'classnames'
+import { CompanyLogoSize } from './CompanyLogo'
 
 const defaultTag = 'div'
 type Available = typeof defaultTag | 'span'
 
 export type CompanyLogoFrameProps<T extends Available = typeof defaultTag> = {
-  size?: 'normal' | 'medium' | 'large'
+  size?: CompanyLogoSize
   children: JSX.Element
 } & BaseProps<T>
 
